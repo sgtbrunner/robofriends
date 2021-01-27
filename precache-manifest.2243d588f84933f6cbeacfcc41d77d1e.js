45ce68ef90f42a1714e833ac642dcf03ad95b82d
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-01-26 22:41:58 -0300

    Updates

---
+++ precache-manifest.2243d588f84933f6cbeacfcc41d77d1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416271ef2356290be1ce59dec0aa2940",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "711064609147cef6135f",
+    "url": "/robofriends/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "5f7649ccbe6a2c4e6d6a",
+    "url": "/robofriends/static/css/main.4663210a.chunk.css"
+  },
+  {
+    "revision": "711064609147cef6135f",
+    "url": "/robofriends/static/js/2.f27a5128.chunk.js"
+  },
+  {
+    "revision": "5f7649ccbe6a2c4e6d6a",
+    "url": "/robofriends/static/js/main.2f285f72.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
