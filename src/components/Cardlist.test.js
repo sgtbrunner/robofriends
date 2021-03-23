b444bf13f6b8260c12c5c4d691d3d6d95bcbef79
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-03-22 22:58:43 -0300

    Moved App container to App folder in src/ | Installed and configured eslint

--- src/components/Cardlist.test.js
+++ src/components/Cardlist.test.js
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme';
 import Cardlist from './Cardlist';
 
 it('expect to render Card component', () => {
-    const mockRobots = [
-        {
-            id: 1,
-            name: 'John Locke',
-            username: 'locke',
-            email: 'locke@gmail.com',
-        }
-    ];
-    expect(shallow(<Cardlist robots={mockRobots}/>).debug()).toMatchSnapshot();
+  const mockRobots = [
+    {
+      id: 1,
+      name: 'John Locke',
+      username: 'locke',
+      email: 'locke@gmail.com',
+    },
+  ];
+  expect(shallow(<Cardlist robots={mockRobots} />).debug()).toMatchSnapshot();
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './App/App';
 import './index.css';
 import 'tachyons';
 
