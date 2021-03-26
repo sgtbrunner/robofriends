9b9dd58264cf80dc5b7870d8a8e8186914ea93e9
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-03-25 23:04:15 -0300

    Reorganized App layout and implementation
    * Updated css styles
    * Created Loading component
    * Created ErrorBoundary component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+
 import Cardlist from '../components/Cardlist/cardlist.component';
+import ErrorBoundary from '../components/Error/error-boundary.component';
+import Loading from '../components/Loading/loading.component';
 import SearchBox from '../components/Searchbox/searchbox.component';
 import Scroll from '../components/Scroll/scroll.component';
 import './App.css';
+
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      error: null,
+      loading: false,
       robots: [],
       searchField: '',
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    this.setState({ loading: true });
+    fetch(USERS_URL)
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+      .then((users) => this.setState({ robots: users }))
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
   }
 
   onSearchChange = (event) => {
@@ -32,16 +42,26 @@ class App extends Component {
   };
 
   render() {
-    if (!this.state.robots.length) {
-      return <h1 className="tc f1">Loading...</h1>;
-    }
+    const { error, loading } = this.state;
+    const { filterRobots, onSearchChange } = this;
+
     return (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <Cardlist robots={this.filterRobots()} />
-        </Scroll>
+        {(loading || error) && (
+          <div className="message-container">
+            { loading && <Loading /> }
+            { error && <ErrorBoundary /> }
+          </div>
+        )}
+        { !error && !loading && (
+          <div>
+            <h1 className="t1">RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange} />
+            <Scroll>
+              <Cardlist robots={filterRobots()} />
+            </Scroll>
+          </div>
+        )}
       </div>
     );
   }
