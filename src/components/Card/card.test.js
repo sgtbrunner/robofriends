9e1705657378b8b14fad7bf810a012424fdd2dcd
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-03-29 20:28:55 -0300

    Fixing linting rules
    * Adding rules ans parser to eslintrc.json
    * Disabling filename extension rules in test files
    * Moving dependencies in package.json
    * Updating test and snapshot in card.test.js

--- src/components/Card/card.test.js
+++ src/components/Card/card.test.js
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './card.component';
 
 it('expect to render Card component', () => {
-  expect(shallow(<Card />).debug()).toMatchSnapshot();
+  expect(
+    shallow(<Card name="Pep Guardiola" email="pep.guardiola@gmail.com" id={0} />).debug()
+  ).toMatchSnapshot();
 });
